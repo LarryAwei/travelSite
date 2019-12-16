7cd779a2635a94808ad209e938984d4779c6012f
zihan <weizh204@163.com>
2019-12-16 20:12:24 +0800

    swiper

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,16 +1,20 @@
 <template>
   <div>
     <home-header/>
+    <home-swiper/>
+    <div>text</div>
   </div>
 
 </template>
 
 <script>
 import homeHeader from './components/header'
+import homeSwiper from './components/Swiper'
 export default {
   name: 'Home',
   components: {
-    homeHeader
+    homeHeader,
+    homeSwiper
   }
 }
 </script>
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <!-- slides -->
+      <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img class="swiper-img"  :src="item.url" alt="">
+      </swiper-slide>
+      <!-- Optional controls -->
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'homeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        // some swiper options/callbacks
+        // 所有的参数同 swiper 官方 api 参数
+        // ...
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [
+        {
+          id: '001',
+          url: 'https://imgs.qunarzz.com/vc/48/4d/58/490ca00c8127e8fe8276ab1a20.jpg_92.jpg'
+        },
+        {
+          id: '002',
+          url: 'https://imgs.qunarzz.com/vc/44/e9/86/95bc36c9e1c06ebd68bdfe222e.jpg_92.jpg'
+        },
+        {
+          id: '003',
+          url: 'https://imgs.qunarzz.com/vc/ad/7c/fe/037d1a400372ff57b0030d1d1c.jpg_92.jpg'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .wrapper >>> .swiper-pagination-bullet-active
+    background white !important
+  .wrapper
+    overflow hidden
+    width 100%
+    height 0
+    padding-bottom 30%
+    background #eaeaea
+    .swiper-img
+      max-width 100%
+</style>
